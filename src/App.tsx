77c621939fd24f25ee472deef33d8c0174c03ad3
Saurabh Dhakne <Saurabh.dhakne@extentia.com>
2025-05-20 10:59:41 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AudioVisualizerParent from './CustomComponents/AudioVisualizer/AudioVisualizerParent'
+
+function App() {
+
+  return (
+    <>
+        <h1>Our Components</h1>
+        <AudioVisualizerParent/>
+    </>
+  )
+}
+
+export default App
